2c432efa68c37f1c579c6f9f87a498332caa0a06
Jeferson Ferreira <jeferson89fs@gmail.com>
2022-05-17 22:39:25 -0300

    arquivos

---
+++ app/Http/DB.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http;
+
+class DB
+{
+
+    private function __construct()
+    {
+        
+    }
+}
--- public/index.php
+++ public/index.php
@@ -7,16 +7,15 @@ require_once("../app/config/config.php");
 require_once("../app/functions/functions.php");
 $prefixo = '';
 
-use App\Model\Conexao;
+// use App\Model\Conexao;
+// $Conexao = new Conexao();
+// $connection = $Conexao->getConnection();
+// $res = $connection->prepare("select * from public.usuario");
+// $res->execute();
 
-$Conexao = new Conexao();
-$connection = $Conexao->getConnection();
-$res = $connection->prepare("select * from public.usuario");
-$res->execute();
 
-
-$dados = $res->fetchAll(PDO::FETCH_CLASS);
-dd($dados);
+// $dados = $res->fetchAll(PDO::FETCH_CLASS);
+// dd($dados);
 
 //implementar .env
 //$ConfigEnv = parse_ini_file('.env');
